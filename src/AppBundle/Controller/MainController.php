41cab92dedaf3ef7e319a4a5482b768eeb529cfd
ngdo-pro <ngdo27@gmail.com>
2017-01-29 17:48:45 +0100

    replaced Default with Main, created a 3-lvl templating

---
+++ src/AppBundle/Controller/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        // replace this example code with whatever you need
+        return $this->render('app/main/index.html.twig');
+    }
+}
