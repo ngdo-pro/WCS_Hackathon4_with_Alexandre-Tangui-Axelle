cd28157b408ec58cc804363a690533e6ee21b063
ngdo-pro <ngdo27@gmail.com>
2017-01-30 16:10:03 +0100

    removed annotation AppBundle and created yml AppBundle instead

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -2,14 +2,10 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MainController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
     public function indexAction()
     {
         return $this->render('app/main/index.html.twig');
